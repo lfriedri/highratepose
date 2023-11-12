475276a96862fef8031295c1ad30830eb41753f0
Lars Friedrich <lars.friedrich.1980@gmx.de>
2023-11-12 14:34:16 +0100

    use argparser

--- benchmark_main.py
+++ benchmark_main.py
@@ -1,48 +1,51 @@
 print('imports')
 
+import argparse
 import os
 import numpy as np
 from time import monotonic
 import tflite_runtime.interpreter as tflite
 
 
+parser = argparse.ArgumentParser('benchmark_main.py',
+                                 description='Profiles movenet inference on Google Coral USB accelerator.')
+parser.add_argument('model_path', nargs='?', default='/home/pi/models/movenet_single_pose_lightning_ptq_edgetpu.tflite')
+
+args = parser.parse_args()
+
 print('loading delegate')
 
 delegate = tflite.load_delegate('libedgetpu.so.1', {})
 
-modelPath = '/home/pi/models'
+modelPath = args.model_path
 
-modelNames = os.listdir(modelPath)
-modelNames = filter(lambda s: s.endswith('edgetpu.tflite'), modelNames)
+print(f'loading model {modelPath}')
+interpreter = tflite.Interpreter(model_path=modelPath,
+                                 experimental_delegates=[delegate])
 
-for name in modelNames:
-    print(f'loading model {name}')
-    interpreter = tflite.Interpreter(model_path=os.path.join(modelPath, name),
-                                        experimental_delegates=[delegate])
+interpreter.allocate_tensors()
 
-    interpreter.allocate_tensors()
+inputInfo = interpreter.get_input_details()
+shape = inputInfo[0]['shape']
 
-    inputInfo = interpreter.get_input_details()
-    shape = inputInfo[0]['shape']
-    
-    print(f'creating random input data of shape: {shape}')
-    
-    input = (np.random.rand(*shape) * 255).astype(np.uint8)
+print(f'creating random input data of shape: {shape}')
 
-    interpreter.set_tensor(inputInfo[0]['index'], input)
+input = (np.random.rand(*shape) * 255).astype(np.uint8)
 
-    nWarmup = 10
-    nMeasure = 100
+interpreter.set_tensor(inputInfo[0]['index'], input)
 
-    print(f'running {nWarmup} warmup cycles')
-    for i in range(nWarmup):
-        interpreter.invoke()
+nWarmup = 10
+nMeasure = 100
 
-    print(f'running {nMeasure} measurement cycles')
-    start = monotonic()
-    for i in range(nMeasure):
-        interpreter.invoke()
+print(f'running {nWarmup} warmup cycles')
+for i in range(nWarmup):
+    interpreter.invoke()
 
-    print(f'average cycle time: {(monotonic() - start) / nMeasure}')
+print(f'running {nMeasure} measurement cycles')
+start = monotonic()
+for i in range(nMeasure):
+    interpreter.invoke()
+
+print(f'average cycle time: {(monotonic() - start) / nMeasure}')
 
 del interpreter
